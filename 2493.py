c486031475edf93dbd5572a83554e5b6ab49bd68
joo <soyong32@naver.com>
2024-02-08 15:02:31 +0900

    2024-02-08

--- 2493.py
+++ 2493.py
@@ -1,15 +1,20 @@
+# import sys
+# input = sys.stdin.readline
+# n = int(input())
 n = 5
 result = [0]*n
-testcase = list(map(int, '6 9 5 7 4'.split()))[::-1]
+testcase = list(map(int, '5 4 3 2 1'.split()))
+test_index = []
+for x in range(n):
+    test_index.append(x)
+stack = []
 i = 1
-top = testcase[-1]
-testcase.pop()
-while len(testcase) > 0:
-    if testcase[-1] <= top:
-        result[i] += k
+stack.append(testcase.pop())
+for i in range(n):
+    if testcase[-1] >= stack[-1]:
+        result[-i] += n - i
+        break
     else:
-        top = testcase[-1]
-        k = testcase.index(top) + 1
-    testcase.pop()
-    i += 1
+        test_index.pop()
+    stack.append(testcase.pop())
 print(result)
