6007681fa6af116d86aca5d07285390e788a5445
joo <soyong32@naver.com>
2024-02-15 17:51:56 +0900

    2024-01-15

--- 9663.py
+++ 9663.py
@@ -1,13 +1,44 @@
 n = int(input())
 board = [i for i in range(1, n**2+1)]
-queens = []
-for queen in board:
-    queens = board.pop()
-    while board:
-        test = board.pop()
-        for i in range(n):
-            if test:
-                pass
+queens = board[:]
+for first in board:
+    queen = first
+    while True:
+        testcase = queens[:]
+        queens = []
+        queen_bool = True
+        for test in testcase:
+            for i in range(-n, n):
+                if test == queen + n * i or test == queen + i or test == queen + n * i + i:
+                    queen_bool = False
+                    break
+                else:
+                    continue
+            if queen_bool:
+                queens.append(test)
+'''    
+    for _ in range(n):
+        board = queens[:]
+        if board:
+            queen = board.pop()
+        else:
+            break
+        queens = []
+        while board:
+            test = board.pop()
+            queen_bool = True
+            for i in range(-n, n):
+                if test == queen + n*i or test == queen + i or test == queen + n*i+i:
+                    queen_bool = False
+                    break
+                else:
+                    continue
+            if queen_bool:
+                queens.append(test)
+    print(queen)
+'''
+
+
 '''
 1  2  3  4
 5  6  7  8 
